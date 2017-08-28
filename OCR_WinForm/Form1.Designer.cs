a37fe01049d4516064953c1e194337a6b176a266
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2017-08-28 23:29:11 +0500

    OCR using tesseract
    A C# WinForm project to do OCR. Does some initial preprocessing before
    displaying some results.
    Assumptions are that the test will be of same font and size.

---
+++ OCR_WinForm/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace OCR_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOCR = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOCR
+            // 
+            this.btnOCR.Location = new System.Drawing.Point(7, 13);
+            this.btnOCR.Name = "btnOCR";
+            this.btnOCR.Size = new System.Drawing.Size(75, 23);
+            this.btnOCR.TabIndex = 0;
+            this.btnOCR.Text = "OCR";
+            this.btnOCR.UseVisualStyleBackColor = true;
+            this.btnOCR.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(9, 58);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResult.Size = new System.Drawing.Size(260, 316);
+            this.txtResult.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 380);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnOCR);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOCR;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ OCR_WinForm/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace OCR_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var image = new Bitmap(openFileDialog.FileName);
+                image = PreprocesImage(image);
+                //image.Save(@"D:\UpWork\OCR_WinForm\Preprocess_BeforeOCR.jpg");
+
+                var ocr = new TesseractEngine(@"D:\UpWork\OCR_WinForm\OCR_WinForm\bin\Debug\tessdata", "eng", EngineMode.TesseractAndCube);
+                var page = ocr.Process(image);
+                var result = page.GetText().ToString().ToLower().Split('\n').Where(x => !string.IsNullOrEmpty(x)).ToArray();
+                foreach (var item in result)
+                    txtResult.Text += item.Trim() + Environment.NewLine;
+            }
+        }
+
+        private Bitmap PreprocesImage(Bitmap image)
+        {
+            //You can change your new color here. Red,Green,LawnGreen any..
+            Color newColor = Color.White;
+            Color actualColor;
+            //make an empty bitmap the same size as scrBitmap
+            image = ResizeImage(image, image.Width * 5, image.Height * 5);
+            //image.Save(@"D:\UpWork\OCR_WinForm\Preprocess_Resize.jpg");
+
+
+            Bitmap newBitmap = new Bitmap(image.Width, image.Height);
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    //get the pixel from the scrBitmap image
+                    actualColor = image.GetPixel(i, j);
+                    // > 150 because.. Images edges can be of low pixel colr. if we set all pixel color to new then there will be no smoothness left.
+                    if (actualColor.R > 135 && actualColor.G > 135 && actualColor.B > 135)
+                        newBitmap.SetPixel(i, j, newColor);
+                    else
+                        newBitmap.SetPixel(i, j, Color.Black);
+                }
+            }
+            return newBitmap;
+        }
+
+        private Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution , image.VerticalResolution * 2);//2,3
+            //image.Save(@"D:\UpWork\OCR_WinForm\Preprocess_HighRes.jpg");
+            
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceOver;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.Clamp);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+    }
+}
